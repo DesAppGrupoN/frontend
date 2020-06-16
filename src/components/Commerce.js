acfcbebd3aced6cca60afcc4a5c4dd9ee4e4dded
Mauro <reekremag@gmail.com>
2020-06-16 15:55:27 -0300

    corrijo tamaño de la imagen

--- src/components/Commerce.js
+++ src/components/Commerce.js
@@ -11,14 +11,14 @@ const Commerce = (props) => {
 
     return (
         <div className={className} >
-            <div className="card horizontal" style={{ "height": "350px", "width": "auto" }}>
+            <div className="card horizontal" style={{ "height": "300px", "width": "auto" }}>
                 <div className="card-image">
-                    <img src={commerce.image} style={{ "height": "200px", "width": "auto" }} />
+                    <img src={commerce.image} style={{ "height": "100%", "width": "220px", }} />
                 </div>
                 <div className="card-stacked">
                     <div className="card-content">
-                        <h5 className="col s12 m4 l9">{commerce.name}</h5>
-                        <p className="col s12 m4 l3 ">{commerce.sector}</p>
+                        <h5 className="col s12 m4 l10">{commerce.name}</h5>
+                        <p className="col s12 m4 l2">{commerce.sector}</p>
                         <h6 className="col s12">{commerce.description}</h6>
                         <h6 className="col s12">{commerce.address}</h6>
                     </div>
