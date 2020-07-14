0f3e2591a72a8a4206aeaf0580ef6fea05e82d12
Mauro Bailon <reekremag@gmail.com>
2020-07-14 10:15:59 -0300

    corrijo server address

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,2 +1,2 @@
-export const server = 'http://desapp-grupon-backend.herokuapp.com';
+export const server = 'https://desapp-grupon-backend.herokuapp.com';
 //export const server = 'http://127.0.0.1:8080';
